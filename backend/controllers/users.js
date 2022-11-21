976266aaddd20e99073f06a0b12b7332d81c9d8b
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-21 14:43:19 +0500

    fix: добавил ответ при авторизации

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -114,7 +114,11 @@ module.exports.login = (req, res, next) => {
     .then((user) => {
       // const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: '7d' });
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'secret', { expiresIn: '7d' });
-      res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true }); // .send({ token })
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        secure: true,
+      }).send({ message: 'Авторизация пройдена!' }); //
     })
     .catch(next);
 };
