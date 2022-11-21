65215c7a9cf59c0c883551bad6e2665bbcde000e
Максим Луговской <lugovskou.myu@yandex.ru>
2022-11-21 13:58:23 +0500

    Revert "fix: cors"
    This reverts commit c3a5db858fe082469b900d15201864165f39552d.

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,4 @@
 const allowedCors = [ // список разрешенных адресов
-  'https://api.lugo.nomoredomains.icu/signin',
-  'http://api.lugo.nomoredomains.icu/signin',
   'https://api.lugo.nomoredomains.icu',
   'http://api.lugo.nomoredomains.icu',
   'https://lugo.nomoredomains.icu',
